872818739fa10d18825e4c6a1936afe86f0238d5
IonRh <kwxos@vip.qq.com>
2025-08-17 00:08:24 +0800

    增加docker构建

--- Docker/main.sh
+++ Docker/main.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 export TZ='Asia/Shanghai'
+
 # 检查/root是否有文件
 if [ "$(ls -A /root/)" ]; then
 	# 有文件则执行TGBot_RSS
-	cd /root && ./TGBot_RSS
+	cd /root && ./TGBot_RSS > /dev/null 2>&1 &
 else
 	# 无文件则移动/app/到/root
 	mv /app/config.json /root/
     mv /app/TGBot_RSS /root/
-    echo "请按需求修改 config.json 配置文件"
-    echo "Modify the config.json configuration file as needed."
+    cd /root/
+    sed -i "s/\"BotToken\": \".*\"/\"BotToken\": \"$BotToken\"/g" config.json
+    sed -i "s/\"ADMINIDS\": [0-9]*/\"ADMINIDS\": $ADMINIDS/g" config.json
+    sed -i "s/\"Cycletime\": [0-9]*/\"Cycletime\": $Cycletime/g" config.json
+    sed -i "s/\"Debug\": \(true\|false\)/\"Debug\": $Debug/g" config.json
+    sed -i "s#\"ProxyURL\": \".*\"#\"ProxyURL\": \"$ProxyURL\"#g" config.json
+    sed -i "s#\"Pushinfo\": \".*\"#\"Pushinfo\": \"$Pushinfo\"#g" config.json
+    ./TGBot_RSS > /dev/null 2>&1 &
 fi
 
 tail -f /dev/null
