fe4e58eb00f8132c8c81aadf23a73bc930b9925b
静雨·安婵 <102129419+IonRh@users.noreply.github.com>
2026-02-11 12:56:48 +0800

    Enhance main.sh with error handling and quoting
    Added error handling and improved variable quoting in config.json updates.

--- Docker/main.sh
+++ Docker/main.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-
 export TZ='Asia/Shanghai'
 
-# 检查/root是否有config.json文件
 if [ -f /root/config.json ]; then
     cp -f /app/TGBot_RSS /root/TGBot_RSS
-    # 有文件则执行TGBot_RSS
     cd /root
-	./TGBot_RSS
+    ./TGBot_RSS
 else
-	# 无文件则移动/app/到/root
-	mv /app/config.json /root/
-    mv /app/TGBot_RSS /root/
-    cd /root/
+    mv /app/config.json /root/ || exit 1
+    cp -f /app/TGBot_RSS /root/TGBot_RSS
+    cd /root
+
+    # 建议加引号防止变量为空或含特殊字符
     sed -i "s/\"BotToken\": \".*\"/\"BotToken\": \"$BotToken\"/g" config.json
     sed -i "s/\"ADMINIDS\": [0-9]*/\"ADMINIDS\": $ADMINIDS/g" config.json
     sed -i "s/\"Cycletime\": [0-9]*/\"Cycletime\": $Cycletime/g" config.json
     sed -i "s/\"Debug\": \(true\|false\)/\"Debug\": $Debug/g" config.json
     sed -i "s#\"ProxyURL\": \".*\"#\"ProxyURL\": \"$ProxyURL\"#g" config.json
     sed -i "s#\"Pushinfo\": \".*\"#\"Pushinfo\": \"$Pushinfo\"#g" config.json
+
     ./TGBot_RSS
 fi
